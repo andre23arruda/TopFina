d5cd7f00b251532a1e4c0994cd195d74c1227121
André Arruda <andre23arruda@gmail.com>
2020-09-07 21:01:33 -0300

    Improve grid for all displays

--- js/navbar-animation.js
+++ js/navbar-animation.js
@@ -1,7 +1,7 @@
 $('#navbar-site').on('show.bs.collapse', function () {
-    $('#header-banner').css('transform', ' translateY(10%)')
+    $('#header-banner').css('transform', ' translateY(10%) translateX(-50%)')
 })
 
 $('#navbar-site').on('hidden.bs.collapse', function () {
-    $('#header-banner').css('transform', ' translateY(-50%)')
+    $('#header-banner').css('transform', ' translateY(-50%) translateX(-50%)')
 })
